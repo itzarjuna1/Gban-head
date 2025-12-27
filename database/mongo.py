b881e29d6f11f2146245a3c6b71d1adcf681d7c4
ARJUN <pachangedinanath@gmail.com>
2025-12-28 00:17:42 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -5,6 +5,6 @@ import config
 client = AsyncIOMotorClient(config.MONGO_URI)
 
 # Use the database specified in DB_NAME
-db = client[config.DARK_DATABASE]
+db = client[config.DB_NAME]
 
-print("[INFO] MongoDB connected to database:", config.DARK_DATABASE)
+print("[INFO] MongoDB connected to database:", config.DB_NAME)
