6155b1eb02845a5a137efa3f4ca02deac66135ff
ARJUN <pachangedinanath@gmail.com>
2025-12-28 09:23:36 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,28 +1,172 @@
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import time
+import random
+from pyrogram import filters
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from youtubesearchpython.__future__ import VideosSearch
 
+from pyrogram.types import InputMediaPhoto
 import config
-from utils.language import lang
-from utils.permissions import sudo_only_message
-from utils.panels import main_panel
+from Gban_head import app  # changed from SONALI to Gban_head
+from Gban_head.misc import _boot_
+from Gban_head.plugins.sudo.sudoers import sudoers_list
+from Gban_head.utils.database import (
+    add_served_chat,
+    add_served_user,
+    blacklisted_chats,
+    get_lang,
+    is_banned_user,
+    is_on_off,
+)
+from Gban_head.utils.decorators.language import LanguageStart
+from Gban_head.utils.formatters import get_readable_time
+from Gban_head.utils.inline import help_pannel, private_panel, start_panel
+from config import BANNED_USERS
+from strings import get_string
 
+#--------------------------
 
-@Client.on_message(filters.private & filters.command("start"))
-async def start_private(app, message):
-    # Check sudo / owner
-    is_sudo = await sudo_only_message(message)
+NEXI_VID = [
+    "https://telegra.ph/file/1a3c152717eb9d2e94dc2.mp4",
+    "https://files.catbox.moe/ln00jb.mp4",
+    "https://graph.org/file/83ebf52e8bbf138620de7.mp4",
+    "https://files.catbox.moe/0fq20c.mp4",
+    "https://graph.org/file/318eac81e3d4667edcb77.mp4",
+    "https://graph.org/file/7c1aa59649fbf3ab422da.mp4",
+    "https://files.catbox.moe/t0nepm.mp4",
+]
 
-    if not is_sudo:
-        # Normal user
-        await message.reply_animation(
-            animation=config.GBAN_LOG_MEDIA,
-            caption=lang("start_normal")
+# ---------- Private Start Handler ----------
+@app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
+@LanguageStart
+async def start_pm(client, message: Message, _):
+    await add_served_user(message.from_user.id)
+
+    if len(message.text.split()) > 1:
+        name = message.text.split(None, 1)[1]
+
+        if name.startswith("help"):
+            keyboard = help_pannel(_)
+            return await message.reply_video(
+                random.choice(NEXI_VID),
+                caption=_["help_1"].format(config.SUPPORT_CHAT),
+                reply_markup=keyboard,
+            )
+
+        if name.startswith("sud"):
+            await sudoers_list(client=client, message=message, _=_)
+            if await is_on_off(2):
+                return await app.send_message(
+                    chat_id=config.LOGGER_ID,
+                    text=f"{message.from_user.mention} just checked sudo list.\nID: {message.from_user.id}\nUsername: @{message.from_user.username}",
+                )
+            return
+
+        if name.startswith("inf"):
+            m = await message.reply_text("🔎")
+            query = name.replace("info_", "", 1)
+            query = f"https://www.youtube.com/watch?v={query}"
+            results = VideosSearch(query, limit=1)
+            for result in (await results.next())["result"]:
+                title = result["title"]
+                duration = result["duration"]
+                views = result["viewCount"]["short"]
+                thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+                channellink = result["channel"]["link"]
+                channel = result["channel"]["name"]
+                link = result["link"]
+                published = result["publishedTime"]
+
+            searched_text = _["start_6"].format(
+                title, duration, views, published, channellink, channel, app.mention
+            )
+            key = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text=_["S_B_8"], url=link),
+                        InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),
+                    ],
+                ]
+            )
+            await m.delete()
+            await app.send_photo(
+                chat_id=message.chat.id,
+                photo=thumbnail,
+                caption=searched_text,
+                reply_markup=key,
+            )
+            if await is_on_off(2):
+                await app.send_message(
+                    chat_id=config.LOGGER_ID,
+                    text=f"{message.from_user.mention} just started bot to check track info.\nID: {message.from_user.id}\nUsername: @{message.from_user.username}",
+                )
+    else:
+        out = private_panel(_)
+        await message.reply_video(
+            random.choice(NEXI_VID),
+            caption=_["start_2"].format(message.from_user.mention, app.mention),
+            reply_markup=InlineKeyboardMarkup(out),
         )
-        return
+        if await is_on_off(2):
+            await app.send_message(
+                chat_id=config.LOGGER_ID,
+                text=f"{message.from_user.mention} just started the bot.\nID: {message.from_user.id}\nUsername: @{message.from_user.username}",
+            )
 
-    # Owner / Sudo panel
-    await message.reply_animation(
-        animation=config.GBAN_LOG_MEDIA,
-        caption=lang("start_owner"),
-        reply_markup=main_panel()
+# ---------- Group Start Handler ----------
+@app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
+@LanguageStart
+async def start_gp(client, message: Message, _):
+    out = start_panel(_)
+    uptime = int(time.time() - _boot_)
+    await message.reply_video(
+        random.choice(NEXI_VID),
+        caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
+        reply_markup=InlineKeyboardMarkup(out),
     )
+    return await add_served_chat(message.chat.id)
+
+# ---------- Welcome New Members ----------
+@app.on_message(filters.new_chat_members, group=-1)
+async def welcome(client, message: Message):
+    for member in message.new_chat_members:
+        try:
+            language = await get_lang(message.chat.id)
+            _ = get_string(language)
+            if await is_banned_user(member.id):
+                try:
+                    await message.chat.ban_member(member.id)
+                except:
+                    pass
+
+            if member.id == app.id:
+                if message.chat.type != ChatType.SUPERGROUP:
+                    await message.reply_text(_["start_4"])
+                    return await app.leave_chat(message.chat.id)
+
+                if message.chat.id in await blacklisted_chats():
+                    await message.reply_text(
+                        _["start_5"].format(
+                            app.mention,
+                            f"https://t.me/{app.username}?start=sudolist",
+                            config.SUPPORT_CHAT,
+                        ),
+                        disable_web_page_preview=True,
+                    )
+                    return await app.leave_chat(message.chat.id)
+
+                out = start_panel(_)
+                await message.reply_video(
+                    random.choice(NEXI_VID),
+                    caption=_["start_3"].format(
+                        message.from_user.mention,
+                        app.mention,
+                        message.chat.title,
+                        app.mention,
+                    ),
+                    reply_markup=InlineKeyboardMarkup(out),
+                )
+                await add_served_chat(message.chat.id)
+                await message.stop_propagation()
+        except Exception as ex:
+            print(ex)
